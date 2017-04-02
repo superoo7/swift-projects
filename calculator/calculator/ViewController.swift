7606282250faff2231bc777abe2ef0820e9a8de3
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-04-02 19:02:30 +0800

    calculator editted

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchDigit(_ sender: UIButton) {
+        var digit = sender.currentTitle
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
