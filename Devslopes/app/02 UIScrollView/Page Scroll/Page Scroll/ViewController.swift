6be85705a60aecfdc8ad3b0afcf34f71c58e50f3
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-05-07 20:26:04 +0800

    editted UIScrollView

--- Devslopes/app/02 UIScrollView/Page Scroll/Page Scroll/ViewController.swift
+++ Devslopes/app/02 UIScrollView/Page Scroll/Page Scroll/ViewController.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = view.frame.midX + view.frame.size.width *  CGFloat(x)
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: 0, y: view.frame.size.height / 2, width: 150, height: 150)
+        }
+        print("Count: \(images.count)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        }
 
 
 }
