87d0746a0225ac25af6849f05a03c7d0d3e923d8
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-05-07 17:23:38 +0800

    done StatePicker

--- Devslopes/app/01 MiraclePill/MiraclePill/MiraclePill/ViewController.swift
+++ Devslopes/app/01 MiraclePill/MiraclePill/MiraclePill/ViewController.swift
@@ -8,19 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alor Gajah", "Ayer Keroh", "Bukit Baru", "Bukit Beruang", "Melaka Raya", "Paya Ikan", "Tebong"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Make background purple
         // self.view.backgroundColor = UIColor.purple
+        
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 
     }
-
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        
+        statePicker.isHidden = false
+    }
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 
 }
 
