7fa52196a9d0644d763c36e81da857aac4e2be95
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-05-08 22:48:51 +0800

    done UIScrollView, left minor error needed to use gesture recognizer

--- Devslopes/app/02 UIScrollView/Page Scroll/Page Scroll/ViewController.swift
+++ Devslopes/app/02 UIScrollView/Page Scroll/Page Scroll/ViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
+        
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
             let imageView = UIImageView(image: image)
@@ -23,15 +30,26 @@ class ViewController: UIViewController {
             
             var newX: CGFloat = 0.0
             
-            newX = view.frame.midX + view.frame.size.width *  CGFloat(x)
+            // newX is center of the screen
+            newX = scrollWidth / 2 + scrollWidth *  CGFloat(x)
+            // contentWidth will increase based on number of newX
+            contentWidth += newX
             
             scrollView.addSubview(imageView)
             
-            imageView.frame = CGRect(x: 0, y: view.frame.size.height / 2, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
+        
+        
+        // Make the view available at the side
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
+        
         print("Count: \(images.count)")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         }
