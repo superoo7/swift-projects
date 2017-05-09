0bc7386b334864e21e60ba8a8bbc7064726e15d3
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-05-10 00:01:30 +0800

    added audio to the app

---
+++ Devslopes/app/05 RetroCalculator/RetroCalculator/RetroCalculator/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by Johnson Lai on 09/05/2017.
+//  Copyright © 2017 johnsonlwh7. All rights reserved.
+//
+
+import UIKit
+import AVFoundation // for audio
+
+class ViewController: UIViewController {
+    
+    var btnSound: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+
+        
+    }
+    
+    @IBAction func numberPressed(sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
+}
+
