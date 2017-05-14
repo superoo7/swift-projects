0503f40f18882f9a542702980e0d78f29c95ef25
Johnson Lai <jlwhoo7@gmail.com>
2017-05-15 00:23:31 +0800

    Done menu bar

--- learn/2/Menu Bar/Menu Bar/ViewController.swift
+++ learn/2/Menu Bar/Menu Bar/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var timer = Timer()
+    
+    @IBAction func cameraBtnPressed(_ sender: Any) {
+        timer.invalidate()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func processTimer() {
+        
+    }
 
 }
 
