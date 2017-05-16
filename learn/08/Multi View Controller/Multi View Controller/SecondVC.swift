22a09f6d168168f7ffc8b5ad6384b9b0c6828dfb
Johnson Lai <jlwhoo7@gmail.com>
2017-05-16 12:39:02 +0800

    Added multi view controller

---
+++ learn/08/Multi View Controller/Multi View Controller/SecondVC.swift
@@ -0,0 +1,36 @@
+//
+//  SecondVC.swift
+//  Multi View Controller
+//
+//  Created by Johnson Lai on 16/05/2017.
+//  Copyright © 2017 johnsonlwh7. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("second view loaded")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
