a69e02f79723263ee3171292305c01257b24ff09
Johnson Lai <jlwhoo7@gmail.com>
2017-05-16 17:12:21 +0800

    Done downloading web content

--- learn/10/Downloading Web Content/Downloading Web Content/ViewController.swift
+++ learn/10/Downloading Web Content/Downloading Web Content/ViewController.swift
@@ -17,12 +17,38 @@ class ViewController: UIViewController {
         
         // web view
         
-        let url = URL(string: "https://www.apple.com/")!
+//        let url = URL(string: "https://www.apple.com/")!
         
 //        webView.loadRequest(URLRequest(url: url))
-        webView.loadHTMLString("<h1>Hello there!</h1>", baseURL: nil)
+        // custom HTML
+//        webView.loadHTMLString("<h1>Hello there!</h1>", baseURL: nil)
         
+        if let url = URL(string: "https://www.stackoverflow.com/") {
+            let request = NSMutableURLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                
+                if error != nil {
+                    print(error)
+                } else {
+                    if let unwrappedData = data {
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        print(dataString)
+                        
+                        DispatchQueue.main.sync(execute: {
+                            // Update UI
+                        })
+                        // custom HTML
+                        // self.webView.loadHTMLString(dataString! as String, baseURL: nil)
+                    }
+                }
+            }
+         task.resume()
         
+        }
+       
     }
 
     override func didReceiveMemoryWarning() {
