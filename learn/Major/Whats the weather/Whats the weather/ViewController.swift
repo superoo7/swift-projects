19d724986b735c52ef05c3a2704218518a213a0b
Johnson Lai <jlwhoo7@gmail.com>
2017-05-16 21:58:10 +0800

    Done Whats the weather

--- learn/Major/Whats the weather/Whats the weather/ViewController.swift
+++ learn/Major/Whats the weather/Whats the weather/ViewController.swift
@@ -8,63 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var cityTextField: UITextField!
     
     @IBAction func submitBtnPressed(_ sender: Any) {
         
+
+            
+    
+        
+        
+        if let url = URL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.replacingOccurrences(of: " ", with: "-") + "/forecasts/latest") {
+            
+            
+            let request = NSMutableURLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest){
+                data, response, error in
+                
+                var message = ""
+                
+                if error != nil {
+                    print(error)
+                } else {
+                    if let unwrappedData = data {
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        var stringSeperator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                        
+                        
+                        if let contentArray = dataString?.components(separatedBy: stringSeperator){
+                            
+                            if contentArray.count > 1 {
+                                stringSeperator = "</span>"
+                                let newContentArray = contentArray[1].components(separatedBy: stringSeperator)
+                                if newContentArray.count > 1 {
+                                    message = newContentArray[0]
+                                    
+                                    message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                    
+                                    print(message)
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+                if message == "" {
+                    message = "The weather there couldn't be found. Please try again."
+                }
+                
+                DispatchQueue.main.sync(execute: {
+                    self.resultLabel.text = message
+                    
+                })
+            }
+            self.view.endEditing(true)
+            task.resume()
+            
+        } else {
+            resultLabel.text = "The weather there couldn't be found. Please try again."
+        }
+        
+
     }
     @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let url = URL(string: "http://www.weather-forecast.com/locations/Kuala-Lumpur/forecasts/latest")!
-        
-        let request = NSMutableURLRequest(url: url)
-        
-        let task = URLSession.shared.dataTask(with: request as URLRequest){
-            data, response, error in
-            
-            var message = ""
-            
-            if error != nil {
-                print(error)
-            } else {
-                if let unwrappedData = data {
-                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                    
-                    var stringSeperator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
-                    
-                    
-                    if let contentArray = dataString?.components(separatedBy: stringSeperator){
-                        
-                        if contentArray.count > 0 {
-                            stringSeperator = "</span>"
-                            let newContentArray = contentArray[1].components(separatedBy: stringSeperator)
-                        if newContentArray.count > 0 {
-                                    message = newContentArray[0]
-                            
-                                message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
-                            
-                                print(message)
-                        
-                            }
-                        }
-                    }
-                }
-            }
-            if message == "" {
-                message = "The weather there couldn't be found. Please try again."
-            }
-            
-            DispatchQueue.main.sync(execute: {
-                self.resultLabel.text = message
-            })
-        }
-        
-        task.resume()
         
         
     }
@@ -72,6 +84,16 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
     }
 
 
