a5f126868184f0c810455cea28bf15551ac43b34
Johnson Lai <jlwhoo7@gmail.com>
2017-05-19 17:07:38 +0800

    Done UI of weather app

---
+++ Devslopes/app/08/weather app/weather app/WeatherVC.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherVC.swift
+//  weather app
+//
+//  Created by Johnson Lai on 19/05/2017.
+//  Copyright © 2017 johnsonlwh7. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
