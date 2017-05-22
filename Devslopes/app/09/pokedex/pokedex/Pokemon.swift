489ec67f9a58a5ee7ae716487a776e4534e8cee5
Johnson Lai <jlwhoo7@gmail.com>
2017-05-23 01:45:11 +0800

    created model file Pokemon

---
+++ Devslopes/app/09/pokedex/pokedex/Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Johnson Lai on 23/05/2017.
+//  Copyright © 2017 johnsonlwh7. All rights reserved.
+//
+
+import Foundation
+
+
+class Pokemon {
+    // id,identifier,species_id,height,weight,base_experience,order,is_default
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
