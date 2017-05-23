06142da61f02e2e49b3cfc6e0b56c8cc69e55b8d
Johnson Lai <jlwhoo7@gmail.com>
2017-05-23 08:19:52 +0800

    created UICollectionViewCell

---
+++ Devslopes/app/09/pokedex/pokedex/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Johnson Lai on 23/05/2017.
+//  Copyright © 2017 johnsonlwh7. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
