a8eb2f3c4cef975445fa7c0c9cc7a3572435c800
Johnson Lai <jlwhoo7@gmail.com>
2017-05-23 14:49:03 +0800

    Make the Pokemon corner radius rounder

--- Devslopes/app/09/pokedex/pokedex/PokeCell.swift
+++ Devslopes/app/09/pokedex/pokedex/PokeCell.swift
@@ -16,10 +16,18 @@ class PokeCell: UICollectionViewCell {
     
     var pokemon: Pokemon!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
     func configureCell(pokemon: Pokemon) {
         self.pokemon = pokemon
         nameLbl.text = self.pokemon.name.capitalized
         thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+        
     }
     
 }
--- Devslopes/app/09/pokedex/pokedex/ViewController.swift
+++ Devslopes/app/09/pokedex/pokedex/ViewController.swift
@@ -8,14 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+        collection.dataSource = self
+        collection.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 105, height: 105)
     }
 }
 
