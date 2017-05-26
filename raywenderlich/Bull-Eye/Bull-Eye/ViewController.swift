c3f7e9d87e5234fc2e6539cfdc3b45db2d73bd82
Johnson Lai <jlwhoo7@gmail.com>
2017-05-26 21:10:21 +0800

    done button part

--- raywenderlich/Bull-Eye/Bull-Eye/ViewController.swift
+++ raywenderlich/Bull-Eye/Bull-Eye/ViewController.swift
@@ -19,6 +19,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello world", message: "This is my app", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
 
 }
