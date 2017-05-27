85c45a8d5f1ed5d20e4aebcc4024be84b866a201
Johnson Lai <jlwhoo7@gmail.com>
2017-05-28 05:16:13 +0800

    created fbMessage

---
+++ other/brian/fbMessage/fbMessage/FriendsVC.swift
@@ -0,0 +1,23 @@
+//
+//  FriendsVC.swift
+//  fbMessage
+//
+//  Created by Johnson Lai on 28/05/2017.
+//  Copyright © 2017 johnsonlwh7. All rights reserved.
+//
+
+import UIKit
+
+class FriendsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.background = UIColor.red
+    }
+
+
+
+
+}
+
